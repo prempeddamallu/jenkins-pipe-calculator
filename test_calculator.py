662a7c00e23379ceced0fa45fcab94a4d8d477cf
prempeddamallu <prem.peddamallu@datagrokr.com>
2024-11-02 18:16:21 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,15 @@ from calculator import add, subtract, multiply
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 2), 4)
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(6, 3), 3)
 
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(3, 3), 9)
 
 if __name__ == '__main__':
     unittest.main()
